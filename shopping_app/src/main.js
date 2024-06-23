443d0d0644fddadfbb1e1c78c132d9eea2de478c
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-06-23 10:41:55 +0900

    json 생성 및 fetch 연결

--- shopping_app/src/main.js
+++ shopping_app/src/main.js
@@ -1,21 +1,20 @@
-// // 1. 옷 리스트 추가
-// const clothesBox = document.querySelector('.main-list');
-// const images = ['blue_p.png', 'blue_s.png', 'blue_t.png', 'pink_p.png', 'pink_s.png', 'pink_t.png', 'yellow_p.png', 'yellow_s.png', 'yellow_t.png'];
+// JSON 파일에서 동적으로 items 받아오기
+function loadItems() {
+    return fetch('data/data.json') // fetch로 json 데이터 받기
+        .then(reponse => reponse.json()) // 받아온 데이터가 성공적이면 json 으로 변환
+        .then(json => json.items); //json 안에 있는 items 를 return
+}
 
-// for (let i = 0; i < images.length * 2; i++) {
-//     const clothesList = document.createElement('LI');
-//     const clothesImg = document.createElement('img');
-//     const chosenImg = images[Math.floor(Math.random() * images.length)];
-//     clothesList.innerText = 'man, large size';
-//     clothesList.appendChild(clothesImg);
-//     clothesBox.appendChild(clothesList);
-//     clothesImg.src = `img/${chosenImg}`;
-// }
 
-// // 2. 카테고리 필터링
-// const blueBtn = document.querySelector('#blueBtn');
-// const yellowBtn = document.querySelector('#yellowBtn');
-// const pinkBtn = document.querySelector('#pinkBtn');
+
+// main
+loadItems()
+    .then(items => {
+        console.log(items);
+        // displayItems(items);
+        // setEventListeners(items);
+    })
+    .catch(console.log)
 
 
 
