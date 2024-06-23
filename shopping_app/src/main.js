b1b94dc29963bd41996e9b7d8a8fbb71951b6aaf
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-06-23 10:21:51 +0900

    html, css 수정

--- shopping_app/src/main.js
+++ shopping_app/src/main.js
@@ -1,21 +1,21 @@
-// 1. 옷 리스트 추가
-const clothesBox = document.querySelector('.main-list');
-const images = ['blue_p.png', 'blue_s.png', 'blue_t.png', 'pink_p.png', 'pink_s.png', 'pink_t.png', 'yellow_p.png', 'yellow_s.png', 'yellow_t.png'];
+// // 1. 옷 리스트 추가
+// const clothesBox = document.querySelector('.main-list');
+// const images = ['blue_p.png', 'blue_s.png', 'blue_t.png', 'pink_p.png', 'pink_s.png', 'pink_t.png', 'yellow_p.png', 'yellow_s.png', 'yellow_t.png'];
 
-for (let i = 0; i < images.length * 2; i++) {
-    const clothesList = document.createElement('LI');
-    const clothesImg = document.createElement('img');
-    const chosenImg = images[Math.floor(Math.random() * images.length)];
-    clothesList.innerText = 'man, large size';
-    clothesList.appendChild(clothesImg);
-    clothesBox.appendChild(clothesList);
-    clothesImg.src = `img/${chosenImg}`;
-}
+// for (let i = 0; i < images.length * 2; i++) {
+//     const clothesList = document.createElement('LI');
+//     const clothesImg = document.createElement('img');
+//     const chosenImg = images[Math.floor(Math.random() * images.length)];
+//     clothesList.innerText = 'man, large size';
+//     clothesList.appendChild(clothesImg);
+//     clothesBox.appendChild(clothesList);
+//     clothesImg.src = `img/${chosenImg}`;
+// }
 
-// 2. 카테고리 필터링
-const blueBtn = document.querySelector('#blueBtn');
-const yellowBtn = document.querySelector('#yellowBtn');
-const pinkBtn = document.querySelector('#pinkBtn');
+// // 2. 카테고리 필터링
+// const blueBtn = document.querySelector('#blueBtn');
+// const yellowBtn = document.querySelector('#yellowBtn');
+// const pinkBtn = document.querySelector('#pinkBtn');
 
 
 
